3dea6ed959b356d6e55b030d646bae3dca5a489f
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-09-26 13:24:54 -0400

    optional access operators

--- src/index.ts
+++ src/index.ts
@@ -131,6 +131,27 @@ let quantity: Quantity = 100;
 function greet(name: string | null | undefined) {
     if (name) console.log(name.toUpperCase());
     else console.log("Hola!");
+    // using unions to include null and undefined means that this code won't throw an error if a null or undefined value is used as a parameter
 }
 
-// using unions to include null and undefined means that this code won't throw an error if a null or undefined value is used as a parameter
+//Optional Chaining
+
+type Customer = {
+    birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+
+//Optional property access operator (?), this means that the function will only run if customer and customer birthday is not null or undefined
+console.log(customer?.birthday?.getFullYear());
+
+//optional element access operator
+// customers?.[0]
+
+// optional call
+let log: any = null;
+log?.("a");
